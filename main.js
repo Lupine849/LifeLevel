7656782a1ea813fec07475d7443539c334f812ac
Kaito.Tamura <tamura.kaito2025@gmail.com>
2026-06-05 19:15:09 +0900

    フォーム送信時のデフォルト動作を無効化

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const taskForm = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task-input');
+
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
